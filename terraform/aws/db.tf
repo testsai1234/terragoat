63fb296606c3760987ecf5bef2261dc8af36b02d
testsai1234 <84868436+testsai1234@users.noreply.github.com>
2021-11-30 01:39:03 +0530

    Update terraform/aws/db.tf
    Co-authored-by: bridgecrew[bot] <60663194+bridgecrew[bot]@users.noreply.github.com>

--- terraform/aws/db.tf
+++ terraform/aws/db.tf
@@ -410,6 +410,9 @@ EOF
     yor_trace            = "f7999d4e-c983-43ee-bd88-7903a6f8483e"
   })
   ebs_optimized = true
+  *_block_device {
+    encrypted = true
+  }
 }
 
 output "db_app_public_dns" {
